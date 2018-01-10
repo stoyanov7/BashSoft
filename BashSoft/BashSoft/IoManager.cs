77892989a2d17e2aac2f8eea78c74a07d3d75001
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-01-10 15:00:57 +0200

    Create method for traversing folders

--- BashSoft/BashSoft/IoManager.cs
+++ BashSoft/BashSoft/IoManager.cs
@@ -1,7 +1,32 @@
 ﻿namespace BashSoft
 {
+    using System.Collections.Generic;
+    using System.IO;
+
     public static class IoManager
     {
-        
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            var initialIdentation = path.Split('\\').Length;
+            var subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+                // Deque the folder at the start ot the queue
+                var currentPath = subFolders.Dequeue();
+                var identation = currentPath.Split('\\').Length - initialIdentation;
+
+                // Print the folder path
+                OutputWriter.WriteMessageOnNewLine($"{new string('-', identation)} {currentPath}");
+
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    // Add all it's subfolders to the end of the queue 
+                    subFolders.Enqueue(directoryPath);
+                }
+            }
+        }
     }
 }
