5cb2763b537d28154c9f203525220889516e0547
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-01-10 15:01:27 +0200

    Test TraverseDirecory in Main()

--- BashSoft/BashSoft/Startup.cs
+++ BashSoft/BashSoft/Startup.cs
@@ -2,8 +2,13 @@
 {
     public class Startup
     {
+        /// <summary>
+        /// Entry point of program.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
         public static void Main(string[] args)
         {
+            IoManager.TraverseDirectory(@"C:\Users\Joro\Desktop");
         }
     }
 }
