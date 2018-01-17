6933b698092979dc2628bb3900a4128367df45a7
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-01-17 10:02:49 +0200

    Add new exceptions

---
+++ BashSoft/BashSoft.Exceptions/InvalidCommandException.cs
@@ -0,0 +1,12 @@
+﻿namespace BashSoft.Exceptions
+{
+    using System;
+
+    public class InvalidCommandException : Exception
+    {
+        public InvalidCommandException(string command)
+            : base($"The command {command} is invalid")
+        {
+        }
+    }
+}
---
+++ BashSoft/BashSoft.Exceptions/InvalidNumberException.cs
@@ -0,0 +1,14 @@
+﻿namespace BashSoft.Exceptions
+{
+    using System;
+
+    public class InvalidNumberException : Exception
+    {
+        private const string NotValidNumber = "{0} is not a valid number.";
+
+        public InvalidNumberException(string message)
+            : base(string.Format(NotValidNumber, message))
+        {
+        }
+    }
+}
