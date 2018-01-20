5572b5b71a5053eb0178cc6d71d7b46729f354e5
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-01-20 17:57:12 +0200

    Test Models

---
+++ BashSoft/Tests/BashSoft.Models.Test/CourseTest/Constructor_Should.cs
@@ -0,0 +1,55 @@
+﻿namespace BashSoft.Models.Test.CourseTest
+{
+    using System;
+    using System.Collections.Generic;
+    using Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void Constructor_InitializeWithValidName_ShouldReturnName()
+        {
+            // Arrange & Act
+            var course = new Course("Valid name");
+
+            // Assert
+            Assert.AreEqual("Valid name", course.Name);
+        }
+
+        [TestMethod]
+        public void Constructor_InitializeWithNullOrEmptyName_ShouldThrowArgumentNullException()
+        {
+            // Arrange & Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Course(null));
+        }
+
+        [TestMethod]
+        public void Constructor_InitializeWithEmptyName_ShouldThrowArgumentException()
+        {
+            // Arrange & Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => new Course(""));
+        }
+
+        [DataTestMethod]
+        [DataRow(" ")]
+        [DataRow("a")]
+        [DataRow("This is very very very very very very very very very long name")]
+        public void Constructor_InitializeInvalidLengthName_ShouldThrowArgumentException(string name)
+        {
+            // Arrange & Act & Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Course(name));
+        }
+
+        [TestMethod]
+        public void Constructor_WhenInvoked_ShouldReturnInstanceOfCourse()
+        {
+            // Arrange & Act
+            var course = new Course("Valid name");
+            
+            // Assert
+            Assert.IsInstanceOfType(course.StudentsByName, typeof(IReadOnlyDictionary<string, IStudent>));
+        }
+    }
+}
---
+++ BashSoft/Tests/BashSoft.Models.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BashSoft.Models.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BashSoft.Models.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0c799bd-4c7e-4264-91c8-4992b5f414e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
